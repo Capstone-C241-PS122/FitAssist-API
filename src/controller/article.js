eedd294da645b14c700716b87f05a6640b57ca1a
Clara <clreyng@gmail.com>
2024-06-08 06:03:14 +0700

    tambahan sedikit

---
+++ src/controller/article.js
@@ -0,0 +1,14 @@
+const prisma = require("../prisma");
+
+const getArticle {
+
+};
+
+const getArticleById {
+
+};
+
+module.exports = {
+    getArticle,
+    getArticleById
+}
--- src/routes/nutrition.js
+++ src/routes/nutrition.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const nutritionCont = require('../controller/nutrition');
+const createNutritionPredict = require('../controller/nutrition');
 
-router.post('/nutrition'); //predict nutrition
+router.post('/nutrition', createNutritionPredict); //predict nutrition
 
 module.exports = router;
