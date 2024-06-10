99a929fbffe7886b2d180f8b3e6aa23aeb8e8cb1
Clara <clreyng@gmail.com>
2024-06-10 21:44:21 +0700

    t

--- src/controller/library.js
+++ src/controller/library.js
@@ -1,5 +1,38 @@
 const prisma = require("../prisma");
 
+const createLibraryVid = async (req, res) => {
+  try {
+    const{type, id} = req.body;
+    const postLibraryVid = await prisma.library.create({
+      data: {type, id},
+    });
+    res.status(201).json({
+      message: "Video disimpan ke Library",
+      postLibraryVid,
+    });
+  }catch (error) {
+    console.error("Gagal menyimpan ke Library:", error);
+    res.status(500).json({ error: "Internal server error"});
+  }
+};
+
+const createLibraryArt = async (req, res) => {
+  try {
+    const{type, id} = req.body;
+    const postLibraryArt = await prisma.library.create({
+      data: {type, id},
+    });
+    res.status(201).json({
+      message: "Artikel disimpan ke Library",
+      postLibraryArt,
+    });
+  }catch (error) {
+    console.error("Gagal menyimpan ke Library:", error);
+    res.status(500).json({ error: "Internal server error"});
+  }
+};
+
+
 // const createLibrary = async (req, res) => {
 //   try {
 //     const {type,vidId} = req.body;
@@ -52,39 +85,39 @@ const prisma = require("../prisma");
 // };
 //akhir modif
 
-//modif 3
-const createLibrary = async (req, res) => {
-  try {
-    const { vidId, articleId } = req.body;
-    let type;
+// //modif 3
+// const createLibrary = async (req, res) => {
+//   try {
+//     const { vidId, articleId } = req.body;
+//     let type;
 
-    if ((vidId && articleId) || (!vidId && !articleId)) {
-      return res.status(400).json({ error: "Please provide either vidId or articleId, not both, and only one." });
-    } else if (vidId) {
-      type = "video";
-    } else if (articleId) {
-      type = "article";
-    }
+//     if ((vidId && articleId) || (!vidId && !articleId)) {
+//       return res.status(400).json({ error: "Please provide either vidId or articleId, not both, and only one." });
+//     } else if (vidId) {
+//       type = "video";
+//     } else if (articleId) {
+//       type = "article";
+//     }
 
-    const postLibrary = await prisma.library.create({
-      data: {
-        type,
-        vidId: vidId || null,       // Use vidId if provided, otherwise null
-        articleId: articleId || null // Use articleId if provided, otherwise null
-      },
-    });
+//     const postLibrary = await prisma.library.create({
+//       data: {
+//         type,
+//         vidId: vidId || null,       // Use vidId if provided, otherwise null
+//         articleId: articleId || null // Use articleId if provided, otherwise null
+//       },
+//     });
 
-    res.status(201).json({
-      message: "Disimpan ke Library",
-      postLibrary,
-    });
-  } catch (error) {
-    console.error("Gagal menyimpan ke Library:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-};
+//     res.status(201).json({
+//       message: "Disimpan ke Library",
+//       postLibrary,
+//     });
+//   } catch (error) {
+//     console.error("Gagal menyimpan ke Library:", error);
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// };
 
-//end modif 3
+// //end modif 3
 
 const getAllLibraries = async (req, res) => {
   try {
@@ -109,7 +142,8 @@ const deleteLibrary = async (req, res) => {
 
 
 module.exports = {
-  createLibrary,
+  createLibraryVid,
+  createLibraryArt,
   getAllLibraries,
-  deleteLibrary,
+  deleteLibrary, //done
 };
