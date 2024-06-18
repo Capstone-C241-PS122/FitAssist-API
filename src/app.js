8d9f96d7ecbe1159da819ee5298a37d6908876db
Clara <claraclaritayng@gmail.com>
2024-06-19 04:45:56 +0700

    Revert "Merge branch 'main' of https://github.com/fauzanzakaria/FitAssist-API"
    This reverts commit 0b157faed0c6355ebdb112a8fcf2130dcbec1c50, reversing
    changes made to fab7d730e52c0e73f118fcebb436bff2d99cea4b.

--- src/app.js
+++ src/app.js
@@ -1,22 +1,23 @@
 const express = require("express");
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 
 
 const feedbackRoute = require('./routes/feedback');
-const nutritionRoute = require('./routes/nutrition');
+const nutritionPredictionRoute = require('./routes/nutrition');
 const articleRoutes = require('./routes/article');
 const vidRoute = require('./routes/video');
 const libRoute = require('./routes/library');
 
 
 app.use('/', articleRoutes);
-app.use('/', feedbackRoute); 
-app.use('/', nutritionRoute); 
-app.use('/', vidRoute); 
+app.use('/', feedbackRoute); //oke
+app.use('/', nutritionPredictionRoute); //oke tp ganti ke get
+app.use('/', vidRoute); //oke
 app.use('/', libRoute); 
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
