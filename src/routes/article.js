d1bf64257037b66bd587de3b41040612ef4e41ee
Clara <claraclaritayng@gmail.com>
2024-06-19 15:51:37 +0700

    .

--- src/routes/article.js
+++ src/routes/article.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { getArticleByBodyPart, getAllArticles, getArticleById  } = require('../controller/article');
-const cacheArticles = require('./cacheMiddleware');
 
 
 router.get('/search', getArticleByBodyPart);
-router.get('/articles', cacheArticles, getAllArticles);
-router.get('/articles/:id', cacheArticles, getArticleById);
+router.get('/articles', getAllArticles);
+router.get('/articles/:id', getArticleById);
 
 module.exports = router;
--- src/routes/video.js
+++ src/routes/video.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const videoCont = require('../controller/video');
-const cacheVideos = require('./cacheMiddleware');
 
-router.get('/video', cacheVideos, videoCont.getAllVideos);
-router.get('/video/:id', cacheVideos, videoCont.getVideoById); 
+router.get('/video', videoCont.getAllVideos);
+router.get('/video/:id', videoCont.getVideoById); 
 router.post('/up/video', videoCont.getVideoByImg);
 
 
